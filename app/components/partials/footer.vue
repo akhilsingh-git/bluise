0ba54eee762a091af1de4d7501ac41d78ecd19ba
Akhil Singh <35478226+akhilsingh-git@users.noreply.github.com>
2023-05-24 12:10:52 +0530

    Update footer.vue

--- app/components/partials/footer.vue
+++ app/components/partials/footer.vue
@@ -24,6 +24,14 @@
           </g>
         </svg>
       </a>
+      <a href="https://linkedin.com/in/your-linkedin-profile">
+        <!-- LinkedIn SVG -->
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="24px" height="32px">
+            <path d="M0 0h24v24H0z" fill="none"/>
+            <path d="M4 4v16h16V4H4zm1.41 2h3.17v2.138c.208-.253.562-.543 1.199-.543.946 0 1.65.617 1.65 1.922V14h3.238V8.984c0-2.17-1.155-3.356-3.014-3.356-1.392 0-2.085.785-2.437 1.204V6H5.41V6z"/>
+        </svg>
+      </a>
+      
     </div>
   </footer>
 </template>
